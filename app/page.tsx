bb9f308c2b1309a517a933656f6c28f4a021fe91
Felix chinonso Emmanuel <Feelchi1star@gmail.com>
2023-10-23 11:47:44 +0100

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -315,16 +315,3 @@ function BlogCard({
     </Card>
   );
 }
-
-async function getData() {
-  const res = await fetch("https://api.example.com/...");
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
